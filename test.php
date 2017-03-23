978d7e724a353b59497921fea6194d750ee9d172
Ricardoc <rcabral.biz@gmail.com>
2017-03-23 14:54:26 +0000

    aaa

--- test.php
+++ test.php
@@ -11,6 +11,8 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+        
+        /* teste */
         require_once '.dat/config.php';
 require_once '.dat/funcs.php';
 
@@ -28,7 +30,7 @@ print "Password: $hash <br />";
 $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
 $query = $pdo->prepare("SELECT * FROM users WHERE login=? AND passwd=?");
 $query->execute([$user,$hash]);
-if ($row = $query->fetch()) {
+if ($row = $query->fetch()) { 
     print "Login ok! <br />";
 }
 else {
